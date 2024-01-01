059668edf106320954ccc9037f9ce14e9a436eb9
V4mpw0L <125161567+V4mpw0L@users.noreply.github.com>
2024-01-01 10:45:56 -0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -4,46 +4,45 @@ import os
 import subprocess
 import time
 import requests
-import sys
-from pytube import YouTube
-from pytube import Playlist
+from pytube import YouTube, Playlist
 from pytube.exceptions import AgeRestrictedError
 from tqdm import tqdm
 from alive_progress import alive_bar
+from colorama import Fore, Style
 from slugify import slugify
 
-# Define colors and testing the update
-class colors:
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    BLUE = '\033[34m'
-    CYAN = '\033[36m'
-    YELLOW = '\033[33m'
-    NORMAL = '\033[0m'
-    BOLD = '\033[1m'
+# Define colors
+class Colors:
+    RED = Fore.RED
+    GREEN = Fore.GREEN
+    BLUE = Fore.BLUE
+    CYAN = Fore.CYAN
+    YELLOW = Fore.YELLOW
+    NORMAL = Style.RESET_ALL
+    BOLD = Style.BRIGHT
 
 # Function to draw a line
 def draw_line():
-    print(colors.BLUE + '=' * os.get_terminal_size().columns + colors.NORMAL)
+    print(Colors.BLUE + '=' * os.get_terminal_size().columns + Colors.NORMAL)
 
 # Function to print a message with a box
 def print_box(message):
     draw_line()
-    print(colors.BLUE + colors.BOLD + message + colors.NORMAL)
+    print(Colors.BLUE + Colors.BOLD + message + Colors.NORMAL)
     draw_line()
 
 # Function to draw a progress bar
 def progress_bar():
-    total = 31
-    with alive_bar(total, bar='classic', spinner='dots_waves', title=f"\033[1;36mDownloading\033[0m", length=40, enrich_print=True, manual=False) as bar:
+    total = 20
+    with alive_bar(total, bar='classic', spinner='dots_waves', title=f"{Colors.BOLD}{Colors.CYAN}Updating...{Colors.NORMAL}", length=40, enrich_print=True, manual=False) as bar:
         for _ in range(total):
             time.sleep(0.1)
             bar()
 
 # Function to check if a command is available
 def command_exists(command):
-    return subprocess.call('type ' + command, shell=True, 
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
+    return subprocess.call('type ' + command, shell=True,
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
 
 # Check if figlet is available
 if not command_exists("figlet"):
@@ -53,9 +52,9 @@ if not command_exists("figlet"):
 # Function to update the system
 def update_system():
     print("Starting system update...")
-    print(colors.BLUE + colors.BOLD)
-    os.system('figlet -f standard "UPDATING..." | lolcat') 
-    print(colors.NORMAL)
+    print(Colors.BLUE + Colors.BOLD)
+    os.system('figlet -f standard "UPDATING..." | lolcat')
+    print(Colors.NORMAL)
     draw_line()
 
     os.system('sudo apt update -y')
@@ -75,10 +74,12 @@ def update_system():
 # Function to ping a website or IP
 def ping_site():
     site = input("Enter the website or IP to ping: ")
-    ip = subprocess.getoutput(f'dig +short {site}')
-
-    print(f"{colors.BOLD}{colors.RED}IP Address: {ip}{colors.NORMAL}")
-    os.system(f'ping -c 5 {site}')
+    try:
+        ip = subprocess.getoutput(f'dig +short {site}')
+        print(f"{Colors.BOLD}{Colors.RED}IP Address: {ip}{Colors.NORMAL}")
+        os.system(f'ping -c 5 {site}')
+    except Exception as e:
+        print(f"Error: {e}")
 
 # Function to geolocate an IP
 def geolocate_ip():
@@ -86,9 +87,9 @@ def geolocate_ip():
     geo = requests.get(f"https://ipinfo.io/{ip}").json()
 
     draw_line()
-    print(f"{colors.GREEN}{colors.BOLD}Geolocation info:{colors.NORMAL}")
+    print(f"{Colors.GREEN}{Colors.BOLD}Geolocation info:{Colors.NORMAL}")
     for key, value in geo.items():
-        print(f"{colors.YELLOW}{key}: {value}{colors.NORMAL}")
+        print(f"{Colors.YELLOW}{key}: {value}{Colors.NORMAL}")
     draw_line()
 
 # Function to update the script from GitHub
@@ -129,7 +130,7 @@ def network_info():
     lines = output.split('\n')
     for line in lines:
         if 'inet' in line:
-            print(colors.GREEN + colors.BOLD + line + colors.NORMAL)
+            print(Colors.GREEN + Colors.BOLD + line + Colors.NORMAL)
         else:
             print(line)
 
@@ -137,9 +138,9 @@ def network_info():
 def download_youtube():
     url = input("Enter the YouTube video URL or playlist URL: ")
     print_box("Choose an option:")
-    print(f"{colors.BLUE}{colors.BOLD}1.| Download Video{colors.NORMAL}")
-    print(f"{colors.BLUE}{colors.BOLD}2.| Download Audio (MP3){colors.NORMAL}")
-    print(f"{colors.BLUE}{colors.BOLD}3.| Cancel{colors.NORMAL}")
+    print(f"{Colors.BLUE}{Colors.BOLD}1.| Download Video{Colors.NORMAL}")
+    print(f"{Colors.BLUE}{Colors.BOLD}2.| Download Audio (MP3){Colors.NORMAL}")
+    print(f"{Colors.BLUE}{Colors.BOLD}3.| Cancel{Colors.NORMAL}")
     draw_line()
     choice = input("Enter your choice: ")
     selected_stream = None  # Initialize selected_stream here
@@ -151,23 +152,24 @@ def download_youtube():
             yt = YouTube(playlist.video_urls[0])
             video_streams = yt.streams.filter(file_extension='mp4', progressive=True)
             for i, stream in enumerate(video_streams, start=1):
-               print(f"{colors.BLUE}{colors.BOLD}{i}.| {stream.resolution} - {stream.filesize / 1024 / 1024:.2f} MB{colors.NORMAL}")
+                print(f"{Colors.BLUE}{Colors.BOLD}{i}.| {stream.resolution} - {stream.filesize / 1024 / 1024:.2f} MB{Colors.NORMAL}")
             selected_stream = int(input("Enter the number of the stream to download: "))
             selected_stream -= 1  # Adjust for 0-based indexing
         for url in playlist.video_urls:
             download_video_or_audio(url, auto_download=True, choice=choice, selected_stream=selected_stream)
     else:
         download_video_or_audio(url, choice=choice, selected_stream=selected_stream)
+
 def download_video_or_audio(url, auto_download=False, choice=None, selected_stream=None):
     yt = YouTube(url)
-    print(f"{colors.GREEN}{colors.BOLD}Title: {yt.title}{colors.NORMAL}")
-    print(f"{colors.GREEN}{colors.BOLD}Duration: {yt.length // 60} minutes {yt.length % 60} seconds{colors.NORMAL}")
+    print(f"{Colors.GREEN}{Colors.BOLD}Title: {yt.title}{Colors.NORMAL}")
+    print(f"{Colors.GREEN}{Colors.BOLD}Duration: {yt.length // 60} minutes {yt.length % 60} seconds{Colors.NORMAL}")
     if choice == '1':
         if selected_stream is None:  # Only ask for user input if selected_stream is None
             print_box("Available Video Streams:")
             video_streams = yt.streams.filter(file_extension='mp4', progressive=True)
             for i, stream in enumerate(video_streams, start=1):
-                print(f"{colors.BLUE}{colors.BOLD}{i}.| {stream.resolution} - {stream.filesize / 1024 / 1024:.2f} MB{colors.NORMAL}")
+                print(f"{Colors.BLUE}{Colors.BOLD}{i}.| {stream.resolution} - {stream.filesize / 1024 / 1024:.2f} MB{Colors.NORMAL}")
             selected_stream = int(input("Enter the number of the stream to download: "))
             selected_stream -= 1  # Adjust for 0-based indexing
         download_video(yt, selected_stream)
@@ -182,13 +184,13 @@ def download_video(yt, selected_stream):
     try:
         video_streams = yt.streams.filter(file_extension='mp4', progressive=True)
     except AgeRestrictedError:
-        print(f"{colors.RED}Age-restricted video: {yt.video_id}. This video can't be accessed without logging in.{colors.NORMAL}")
+        print(f"{Colors.RED}Age-restricted video: {yt.video_id}. This video can't be accessed without logging in.{Colors.NORMAL}")
         return
     if selected_stream is not None:
         video = video_streams[selected_stream]
     else:
         video = video_streams.first()
-    print(f"{colors.GREEN}{colors.BOLD}Downloading: {video.resolution} - {video.filesize / 1024 / 1024:.2f} MB{colors.NORMAL}")
+    print(f"{Colors.GREEN}{Colors.BOLD}Downloading: {video.resolution} - {video.filesize / 1024 / 1024:.2f} MB{Colors.NORMAL}")
     response = requests.get(video.url, stream=True)
     total_size = int(response.headers.get('content-length', 0))
     # Use tqdm to create a progress bar
@@ -201,47 +203,47 @@ def download_video(yt, selected_stream):
                 bar.update(len(data))
                 f.write(data)
     print_box(f"Video downloaded successfully as {slugify(yt.title)}_video.mp4 in the PlaylistVideos folder")
+
 def download_audio(yt):
     try:
         audio_streams = yt.streams.filter(only_audio=True)
     except AgeRestrictedError:
-        print(f"{colors.RED}Age-restricted video: {yt.video_id}. This video can't be accessed without logging in.{colors.NORMAL}")
+        print(f"{Colors.RED}Age-restricted video: {yt.video_id}. This video can't be accessed without logging in.{Colors.NORMAL}")
         return
     audio = audio_streams[0]
-    print(f"{colors.GREEN}{colors.BOLD}Downloading audio: {audio.abr} - {audio.filesize / 1024 / 1024:.2f} MB{colors.NORMAL}")
+    print(f"{Colors.GREEN}{Colors.BOLD}Downloading audio: {audio.abr} - {audio.filesize / 1024 / 1024:.2f} MB{Colors.NORMAL}")
     response = requests.get(audio.url, stream=True)
     total_size = int(response.headers.get('content-length', 0))
     # Use tqdm to create a progress bar
     with tqdm(total=total_size, unit='b', unit_scale=True, unit_divisor=1024) as bar:
-        folder_path = os.path.join(os.getcwd(), 'AudiosDownloads')  # Create a folder for audio downloads
+        folder_path = os.path.join(os.getcwd(), 'AudiosDownloads')
         os.makedirs(folder_path, exist_ok=True)
-        file_path = os.path.join(folder_path, f"{slugify(yt.title)}_audio.mp3")  # Save the MP3 file in the new folder
+        file_path = os.path.join(folder_path, f"{slugify(yt.title)}_audio.mp3")
         with open(file_path, 'wb') as f:
             for data in response.iter_content(chunk_size=1024):
                 bar.update(len(data))
                 f.write(data)
     print_box(f"Audio downloaded successfully as {slugify(yt.title)}_audio.mp3 in the AudioDownloads folder")
 
-
 # Menu
 while True:
     draw_line()
-    print(colors.CYAN + colors.BOLD)
-    os.system('figlet -f standard " M E N U " | lolcat') 
-    print(colors.NORMAL)
+    print(Colors.CYAN + Colors.BOLD)
+    os.system('figlet -f standard " M E N U " | lolcat')
+    print(Colors.NORMAL)
     draw_line()
-    print(f"{colors.CYAN}{colors.BOLD}1.| Update the system{colors.NORMAL}")
-    print(f"{colors.BLUE}{colors.BOLD}2.| Ping a website or IP{colors.NORMAL}")
-    print(f"{colors.BLUE}{colors.BOLD}3.| Geolocate an IP{colors.NORMAL}")
-    print(f"{colors.BLUE}{colors.BOLD}4.| Disk Usage{colors.NORMAL}")
-    print(f"{colors.BLUE}{colors.BOLD}5.| Memory Usage{colors.NORMAL}")
-    print(f"{colors.BLUE}{colors.BOLD}6.| System Uptime{colors.NORMAL}")
-    print(f"{colors.BLUE}{colors.BOLD}7.| List Running Processes{colors.NORMAL}")
-    print(f"{colors.BLUE}{colors.BOLD}8.| Network Information{colors.NORMAL}")
-    print(f"{colors.BLUE}{colors.BOLD}9.| System Information{colors.NORMAL}")
-    print(f"{colors.GREEN}{colors.BOLD}10.| Download YouTube Video or MP3{colors.NORMAL}")
-    print(f"{colors.YELLOW}{colors.BOLD}11.| Update the Script{colors.NORMAL}")
-    print(f"{colors.RED}{colors.BOLD}12.| Exit{colors.NORMAL}")
+    print(f"{Colors.CYAN}{Colors.BOLD}1.| Update the system{Colors.NORMAL}")
+    print(f"{Colors.BLUE}{Colors.BOLD}2.| Ping a website or IP{Colors.NORMAL}")
+    print(f"{Colors.BLUE}{Colors.BOLD}3.| Geolocate an IP{Colors.NORMAL}")
+    print(f"{Colors.BLUE}{Colors.BOLD}4.| Disk Usage{Colors.NORMAL}")
+    print(f"{Colors.BLUE}{Colors.BOLD}5.| Memory Usage{Colors.NORMAL}")
+    print(f"{Colors.BLUE}{Colors.BOLD}6.| System Uptime{Colors.NORMAL}")
+    print(f"{Colors.BLUE}{Colors.BOLD}7.| List Running Processes{Colors.NORMAL}")
+    print(f"{Colors.BLUE}{Colors.BOLD}8.| Network Information{Colors.NORMAL}")
+    print(f"{Colors.BLUE}{Colors.BOLD}9.| System Information{Colors.NORMAL}")
+    print(f"{Colors.GREEN}{Colors.BOLD}10.| Download YouTube Video or MP3{Colors.NORMAL}")
+    print(f"{Colors.YELLOW}{Colors.BOLD}11.| Update the Script{Colors.NORMAL}")
+    print(f"{Colors.RED}{Colors.BOLD}12.| Exit{Colors.NORMAL}")
     draw_line()
     choice = input("Enter your choice: ")
 
